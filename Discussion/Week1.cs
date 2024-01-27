4a69c3b9189942cbf3fd897b5a5a791cc0ba0b21
cngai11 <157764877+cngai11@users.noreply.github.com>
2024-01-26 20:55:30 -0500

    Update Week1.cs
    updated

--- Discussion/Week1.cs
+++ Discussion/Week1.cs
@@ -10,7 +10,7 @@
             Console.WriteLine("Test by Lun");
             Console.WriteLine("Test by Chun");
             Console.WriteLine("Test by Lun2");
-            Console.WriteLine("Testing by Kan v3  ");
+            Console.WriteLine("Testing by Kan v3");
         }
     }
 }
